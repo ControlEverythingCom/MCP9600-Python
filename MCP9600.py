2e74f162520217ec09fd55ee15a2f36ad289074a
telliottoscoela <travis"controlanything.com>
2017-01-09 21:03:03 +0000

    updated to use bus.read_i2c_block_data

--- MCP9600.py
+++ MCP9600.py
@@ -9,20 +9,22 @@ bus.write_byte_data(address, 0xC0, 0x00)
 
 time.sleep(0.5)
 
-bus.write_byte(address, 0xC1)
+data = bytearray()
 
+bus.write_byte(address, 0xC1)
 time.sleep(0.5)
 
-data0 = bus.read_byte(address)
+data = bus.read_i2c_block_data(address, 2)
 
-data1 = bus.read_byte(address)
+print data[0]
+print data[1]
 
-if((data0 & 0x80) == 0x80):
-  data0 = data0 & 0x7F
-  temp = 1024 - (data0 * 16 + data1/16)
-  temp = (temp * 1.8)+32
-  print temp
+if((data[0] & 0x80) == 0x80):
+    data[0] = data[0] &0x7F
+    temp = 1024 - (data[0] * 16 + data[1]/16)
+    temp = (temp * 1.8)+32
+    print temp
 else:
-  temp = data0 * 16 + data1/16
-  temp = (temp * 1.8)+32
-  print temp
+    temp = data[0] * 16 + data[1] / 16
+    temp = (temp * 1.8)+32
+    print temp
